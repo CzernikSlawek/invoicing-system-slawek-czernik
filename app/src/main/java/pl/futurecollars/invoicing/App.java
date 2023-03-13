f8bd4754d1ee45a674518725ffe04cbf30c9541a
CzernikSlawek <adres_emailczernik.slawomir2@gmail.com>
2023-03-13 23:02:03 +0100

    lombok, test, textutils added

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -4,13 +4,27 @@
 
 package pl.futurecollars.invoicing;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import pl.futurecollars.invoicing.model.Invoice;
+import pl.futurecollars.invoicing.utils.TextUtils;
+
 public class App {
 
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+
+    System.out.println(new App().getGreeting());
+
+    List<Invoice> invoices = new ArrayList<>();
+
+    Invoice invoice1 = new Invoice(1L, LocalDateTime.now(), "c1", "c2");
+
+    String upperCaseText = TextUtils.capitalizeText("text");
+  }
+
 }
